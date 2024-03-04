13bedd9ab759e97f342bd76ace623f61592f0054
fjhus16 <65453275+huseyinonalcom@users.noreply.github.com>
2024-03-04 17:55:57 +0100

    Update getproducts.tsx

--- pages/api/products/public/getproducts.tsx
+++ pages/api/products/public/getproducts.tsx
@@ -99,7 +99,7 @@ export default async function getProducts(req, res) {
     }
 
     fetchUrl += `populate[document_products][fields][0]=amount&populate[product_extra][fields][0]=*&populate[shelves][fields][0]=stock&fields[0]=name&populate[category][fields][0]=name&fields[2]=productLine&fields[3]=internalCode&fields[5]=value&fields[8]=description&populate[shelves][populate][supplier_order_products][fields][0]=amountOrdered&populate[shelves][populate][supplier_order_products][fields][1]=amountDelivered&fields[1]=depth&fields[4]=priceBeforeDiscount&fields[6]=width&fields[7]=height&fields[9]=material&fields[10]=color&populate[images][fields][0]=url&pagination[page]=${pageParam}${
-      sortParam ? `&sort=${sortParam}` : ""
+      sortParam ? `&sort[0]=${sortParam}` : ""
     }${countParam ? `&pagination[pageSize]=${countParam}` : ""}`;
 
     const response = await fetch(fetchUrl, {
@@ -110,9 +110,23 @@ export default async function getProducts(req, res) {
 
     const data = await response.json();
 
-    const sortedData: Product[] = data["data"].map(
-      (productData) => productData as Product
-    );
+    // sort productData array on product.product_extra.new from true to false
+    const sortedData: Product[] = data["data"]
+      .map((productData) => productData as Product)
+      .sort((a, b) => {
+        // Assuming product_extra.new is a boolean property
+        const isNewA = a.product_extra.new;
+        const isNewB = b.product_extra.new;
+
+        // If both are new or both are not new, maintain their original order
+        if (isNewA === isNewB) {
+          return 0;
+        }
+
+        // Sort new items first (true comes before false)
+        return isNewA ? -1 : 1;
+      });
+
     const totalPages = data["meta"]["pagination"]["pageCount"];
 
     var minValueFromAPI = 20;
