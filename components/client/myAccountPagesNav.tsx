d4b753463a07fe2a2f724c7ef56065a1d6921d84
fjhus16 <65453275+huseyinonalcom@users.noreply.github.com>
2024-03-08 17:59:57 +0100

    Update myAccountPagesNav.tsx

--- components/client/myAccountPagesNav.tsx
+++ components/client/myAccountPagesNav.tsx
@@ -3,16 +3,27 @@ import router, { useRouter } from "next/router";
 import Link from "next/link";
 import useTranslation from "next-translate/useTranslation";
 import { ClientContext } from "../../api/providers/clientProvider";
+import { AutoTextSize } from "auto-text-size";
 
 export default function MyAccountPagesNav() {
-  const { t, lang } = useTranslation("common");
+  const { t } = useTranslation("common");
   const { pathname } = useRouter(); // Get the current pathname
-  const { clearClient } = useContext(ClientContext);
+  const router = useRouter();
+  const { client, clearClient } = useContext(ClientContext);
+
+  console.log(client);
 
   const getNavLinkClass = (href) => {
     const baseClass =
       "font-bold py-1 pl-1 duration-300 w-full hover:bg-gray-200";
     return href === pathname ? `${baseClass} bg-gray-200` : baseClass;
+  };
+
+  const handleLogOut = async (event) => {
+    event.preventDefault();
+    clearClient();
+    await fetch("/api/client/client/logout").then(() => {});
+    router.push("/");
   };
 
   useEffect(() => {
@@ -27,9 +38,34 @@ export default function MyAccountPagesNav() {
 
   return (
     <div className="flex flex-col items-left w-[200px] justify-start pr-2 border-r border-gray-400 flex-shrink-0 print:hidden">
-      <h2 className="font-bold text-2xl pb-2 border-gray-400 border-b mb-4">
+      <h2 className="font-bold text-2xl pb-2 border-gray-400 border-b mb-2">
         {t("Ma Compte")}
       </h2>
+      {client && (
+        <>
+          <h2 className="font-bold py-1 pl-1 w-full">
+            <AutoTextSize maxFontSizePx={12}>
+              {client.client_info.firstName + " " + client.client_info.lastName}
+            </AutoTextSize>
+          </h2>
+          <Link
+            href={"/"}
+            as={"/"}
+            onClick={handleLogOut}
+            className="font-bold py-1 pl-1 duration-300 w-full hover:bg-gray-200"
+          >
+            <AutoTextSize maxFontSizePx={12}>
+              {t("Not") +
+                " " +
+                client.client_info.firstName +
+                " " +
+                client.client_info.lastName}
+              ?
+            </AutoTextSize>
+          </Link>
+        </>
+      )}
+      <Link href={"/"} onClick={handleLogOut}></Link>
       {/* <Link href={"/account/myaccount"} className={getNavLinkClass("/account/myaccount")}>
         {t("Tableau du bord")}
       </Link> */}
