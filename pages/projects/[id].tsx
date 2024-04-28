ce28a493c5f570fb0e5b5b91a9450f2d97902214
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-04-28 07:45:09 +0300

    s

---
+++ pages/projects/[id].tsx
@@ -0,0 +1,100 @@
+import { getAllCategoriesFlattened } from "../api/categories/public/getallcategoriesflattened";
+import {
+  getAllProductIDs,
+  getProductByID,
+  getProducts,
+} from "../../api/calls/productCalls";
+import ProductPreview from "../../components/products/product-preview";
+import ProductButtons from "../../components/products/product-buttons";
+import { Fragment, useEffect, useRef, useState } from "react";
+import useTranslation from "next-translate/useTranslation";
+import { Product } from "../../api/interfaces/product";
+import Layout from "../../components/public/layout";
+import { ArrowLeft, Facebook } from "react-feather";
+import Meta from "../../components/public/meta";
+import Image from "next/image";
+import Head from "next/head";
+import Link from "next/link";
+import { useDragScroll } from "../../components/common/use-drag-scroll";
+import { getProjectByID } from "../api/projects/public/getprojectbyid";
+import { getAllProjectIDs } from "../api/projects/public/getallprojectids";
+
+type Props = {
+  project;
+};
+
+const ProjectPage = ({ project }: Props) => {
+  const { t, lang } = useTranslation("common");
+
+  return (
+    <Layout>
+      <Head>
+        <title>Horeca Depot</title>
+        <meta name="description" content="" />
+        <meta name="language" content={lang} />
+      </Head>
+
+      <h1 className="bg-black pt-2 text-center text-4xl font-bold text-white">
+        {project.title}
+      </h1>
+      <div className="flex h-[70px] flex-col justify-around bg-black shadow-lg">
+        <div className="text-md flex flex-row items-center justify-center font-bold text-gray-300">
+          <Link href="/" className="duration-700 hover:text-white">
+            {t("Home")}
+          </Link>
+          <p className="mx-2 font-bold">/</p>
+          <p className="text-white underline decoration-orange-400 decoration-solid decoration-4 underline-offset-8">
+            {project.title}
+          </p>
+        </div>
+      </div>
+      <div className="mx-auto mb-2 flex w-[95%] flex-col items-center justify-center bg-white p-3 shadow-md">
+        <div className="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-2">
+          {project.images.map((image, index) => (
+            <div key={index} className="relative min-h-[200px] min-w-[200px]">
+              <Image
+                sizes="(max-width: 768px) 95vw, (max-width: 1024px) 48vw, 20vw"
+                src={"https://hdapi.huseyinonalalpha.com" + image.url}
+                fill
+                style={{ objectFit: "contain" }}
+                alt={`${image.name.split(".")[0]}`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+type Params = {
+  params: {
+    id: string;
+  };
+};
+
+export const getStaticProps = async ({ params }: Params) => {
+  const project = await getProjectByID(Number.parseInt(params.id));
+
+  return {
+    props: { project },
+    revalidate: 900,
+  };
+};
+
+export async function getStaticPaths({}) {
+  const result = await getAllProjectIDs();
+  const allProductIDs: number[] = result;
+  return {
+    paths: allProductIDs.map((ID) => {
+      return {
+        params: {
+          id: ID.toString(),
+        },
+      };
+    }),
+    fallback: "blocking",
+  };
+}
+
+export default ProjectPage;
