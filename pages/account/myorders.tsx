344d0c231b04569a8854b9302587c6d39c3477f7
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-05-18 04:40:47 +0300

    Update myorders.tsx

--- pages/account/myorders.tsx
+++ pages/account/myorders.tsx
@@ -9,6 +9,8 @@ import Meta from "../../components/public/meta";
 import { useEffect, useState } from "react";
 import LoadingIndicator from "../../components/common/loadingIndicator";
 import OrderPreview from "../../components/orders/order-preview";
+import Link from "next/link";
+import { ChevronLeft } from "react-feather";
 
 export default function MyOrders() {
   const { t, lang } = useTranslation("common");
@@ -43,8 +45,14 @@ export default function MyOrders() {
         <meta name="language" content={lang} />
       </Head>
       <div className="mx-auto flex w-full flex-row items-start justify-start">
-        <div className="flex w-full flex-col items-center gap-2">
-          <h1 className="text-3xl font-bold">{t("My Orders")}</h1>
+        <div className="flex w-full flex-col items-center gap-2 pt-2">
+        <div className="mb-3 flex flex-row justify-start w-full items-center gap-4">
+          <Link href="/account/myaccount" className="group flex text-black">
+            <ChevronLeft className="group-hover:animate-bounceHorizontal" />
+            <p>{t("Back to Account")}</p>
+          </Link>
+          <h2 className="text-xl font-semibold">{t("My Orders")}</h2>
+        </div>
           <div className="grid-cols grid w-full grid-cols-1 justify-between gap-2 xl:grid-cols-2 2xl:grid-cols-3">
             {isLoading ? (
               <LoadingIndicator />
