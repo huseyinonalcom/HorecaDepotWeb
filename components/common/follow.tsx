a175630e9b664fd1f683c37115f3bed1d982763f
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-05-18 14:09:54 +0300

    s

--- components/common/follow.tsx
+++ components/common/follow.tsx
@@ -5,27 +5,42 @@ import Link from "next/link";
 const Follow = () => {
   const iconSize: number = 36;
   return (
-    <div className="grid grid-cols-4 gap-8">
+    <div className="grid grid-cols-4 gap-8 items-center">
       <Link
         href={"https://www.facebook.com/HorecaDepotBelgium/"}
         aria-label="Link to Facebook"
         target="_blank"
       >
-        <Facebook width={iconSize} height={iconSize} />
+        <Image
+          src={"/assets/header/facebook.png"}
+          alt="Facebook"
+          width={iconSize}
+          height={iconSize}
+        />
       </Link>
       <Link
         aria-label="Link to Instagram"
         href={"https://www.instagram.com/horecadepot.be/"}
         target="_blank"
       >
-        <Instagram width={iconSize} height={iconSize} />
+        <Image
+          src={"/assets/header/instagram.png"}
+          alt="Instagram"
+          width={iconSize}
+          height={iconSize}
+        />
       </Link>
       <Link
         aria-label="Link to Youtube"
         href={"https://www.youtube.com/@HorecaDepot/"}
         target="_blank"
       >
-        <Youtube width={iconSize} height={iconSize} />
+        <Image
+          src={"/assets/header/youtube.png"}
+          alt="Youtube"
+          width={iconSize}
+          height={iconSize}
+        />
       </Link>
       <Link
         aria-label="Link to Pinterest"
@@ -33,7 +48,7 @@ const Follow = () => {
         target="_blank"
       >
         <Image
-          src={"/assets/img/pinterest.svg"}
+          src={"/assets/header/pinterest.png"}
           alt="Pinterest"
           width={iconSize}
           height={iconSize}
