3c43559797d73b869b688c44fa19f0113163ac2b
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-05-29 17:56:01 +0300

    s

--- pages/api/universal/admin/puttoapi.ts
+++ pages/api/universal/admin/puttoapi.ts
@@ -2,28 +2,32 @@ import statusText from "../../../../api/statustexts";
 import getAuthCookie from "../../cookies";
 
 export async function putToAPI({ req, res }) {
-  const authToken = getAuthCookie({ type: "admin", req });
-  const collection = req.query.collectiontoput;
-  const id = req.query.idtoput;
-  const bodyToPut = req.body;
+  try {
+    const authToken = getAuthCookie({ type: "admin", req });
+    const collection = req.query.collectiontoput;
+    const id = req.query.idtoput;
+    const bodyToPut = req.body;
 
-  const request = await fetch(
-    `${process.env.API_URL}/api/${collection}${id ? `/${id}` : ""}`,
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${authToken}`,
+    const request = await fetch(
+      `${process.env.API_URL}/api/${collection}${id ? `/${id}` : ""}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authToken}`,
+        },
+        body: JSON.stringify({
+          data: bodyToPut,
+        }),
       },
-      body: JSON.stringify({
-        data: bodyToPut,
-      }),
-    },
-  );
+    );
 
-  if (request.ok) {
-    return true;
-  } else {
+    if (request.ok) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (_) {
     return false;
   }
 }
