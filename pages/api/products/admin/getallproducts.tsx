d0f5c709e5523a7ecd6ffeea55ea81aacab6504d
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-06-07 23:55:47 +0300

    d

--- pages/api/products/admin/getallproducts.tsx
+++ pages/api/products/admin/getallproducts.tsx
@@ -1,13 +1,10 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest } from "next";
 import statusText from "../../../../api/statustexts";
 
 const fetchUrl = `${process.env.API_URL}/api/products?filters[deleted][$eq]=false&fields[0]=name&fields[1]=supplierCode&fields[2]=internalCode&fields[3]=value&fields[4]=depth&fields[5]=width&fields[6]=height&fields[7]=material&fields[8]=color&fields[9]=priceBeforeDiscount&fields[10]=active&populate[product_extra][fields][0]=weight&populate[product_extra][fields][1]=per_box&populate[product_extra][fields][2]=packaged_weight&populate[product_extra][fields][3]=packaged_dimensions&populate[product_extra][fields][4]=seat_height&populate[product_extra][fields][5]=diameter&populate[product_extra][fields][6]=surface_area&populate[product_extra][fields][7]=packaged_weight_net&populate[product_extra][fields][8]=barcode&populate[product_extra][fields][9]=armrest_height&populate[categories][fields][0]=name&populate[shelves][fields][0]=stock&populate[shelves][populate][establishment][fields][0]=id&populate[reservations][fields][0]=client_name&populate[reservations][fields][1]=amount&populate[reservations][fields][2]=is_deleted&populate[images][fields][0]=url`;
 
-export default async function getAllProducts(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  if (req.method === "GET") {
+export async function getAllProducts(req: NextApiRequest) {
+  try {
     const cookies = req.cookies;
     const authToken = cookies.j;
     const page = "&pagination[page]=" + (req.query.page ?? 1);
@@ -30,7 +27,7 @@ export default async function getAllProducts(
     }
 
     if (!authToken) {
-      return res.status(401).json(statusText[401]);
+      return null;
     }
 
     try {
@@ -42,17 +39,42 @@ export default async function getAllProducts(
           },
         },
       );
-
-      if (!request.ok) {
-        return res.status(400).json(statusText[400]);
-      }
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+      console.log("--------------------------------------------------------");
+      console.log(
+        fetchUrl + category + page + sort + supplier + search + pageSize,
+      );
+      console.log("--------------------------------------------------------");
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 
       const response = await request.json();
-      return res.status(200).json(response);
+
+      console.log(response);
+
+      if (!request.ok) {
+        return null;
+      }
+
+      return response;
     } catch (error) {
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export default async function handler(req, res) {
+  try {
+    const response = await getAllProducts(req);
+
+    if (!response) {
       return res.status(500).json(statusText[500]);
     }
-  } else {
-    return res.status(405).json(statusText[405]);
+
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(500).json(statusText[500]);
   }
 }
