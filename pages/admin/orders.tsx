c39e688f2d57373caf143b7b4b3bea21cdd63479
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-07-19 20:20:19 +0300

    s

--- pages/admin/orders.tsx
+++ pages/admin/orders.tsx
@@ -77,139 +77,128 @@ export default function Orders() {
         <title>{t("document_orders")}</title>
         <meta name="language" content={lang} />
       </Head>
-      <div className="flex w-full flex-col items-center pt-2">
-        <div>
-          <div className="flex-shrink-1 flex w-full flex-col items-center overflow-y-hidden pt-1">
-            <div className="flex max-w-full flex-col overflow-x-auto overflow-y-auto">
-              <table className="relative w-full bg-gray-100 p-2 shadow-lg">
-                <thead className="sticky top-0 bg-[#c0c1c3]">
-                  <tr>
-                    <th>{t("document_order")} #</th>
-                    <th>{t("Date")}</th>
-                    <th>{t("Client")}</th>
-                    <th>{t("Total")}</th>
-                    <th>{t("To pay")}</th>
-                    <th>{t("Paid")}</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {allOrders.map((order, index) => (
-                    <tr
-                      key={index}
-                      className={`cursor-pointer ${
-                        index % 2 === 0 ? "bg-slate-300" : ""
-                      }`}
-                      onDoubleClick={() =>
-                        router.push(`/admin/order?id=${order.id}`)
-                      }
-                      onMouseOver={(e) =>
-                        e.currentTarget.classList.add("hover:bg-slate-500")
-                      }
-                      onMouseOut={(e) =>
-                        e.currentTarget.classList.remove("hover:bg-slate-500")
-                      }
-                    >
-                      <td>{order.prefix + order.number}</td>
-                      <td>{formatDateAPIToBe(order.date)}</td>
-                      <td>{`${order.client.firstName} ${order.client.lastName}`}</td>
-                      <td>
-                        €{" "}
-                        {order.document_products
-                          .reduce(
-                            (total, product) => total + product.subTotal,
-                            0,
-                          )
-                          .toFixed(2)
-                          .replaceAll(".", ",")}
-                      </td>
-                      <td>
-                        €{" "}
-                        {(
-                          order.document_products.reduce(
-                            (total, product) => total + product.subTotal,
-                            0,
-                          ) -
-                          order.payments.reduce(
-                            (total, payment) =>
-                              total +
-                              (payment.deleted || !payment.verified
-                                ? 0
-                                : payment.value),
-                            0,
-                          )
-                        )
-                          .toFixed(2)
-                          .replaceAll(".", ",")}
-                      </td>
-                      <td>
-                        {order.document_products.reduce(
-                          (total, product) => total + product.subTotal,
-                          0,
-                        ) -
-                          order.payments.reduce(
-                            (total, payment) =>
-                              total +
-                              (payment.deleted || !payment.verified
-                                ? 0
-                                : payment.value),
-                            0,
-                          ) >
-                        0 ? (
-                          <X className="text-red-500" />
-                        ) : (
-                          <Check className="text-green-500" />
-                        )}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <>
-            {allOrders.length > 0 ? (
-              <div className="mb-4 flex flex-row justify-center px-6">
-                <div className="mt-2">
-                  <div className="flex items-center justify-center space-x-1">
-                    <button
-                      className="border p-2  hover:bg-gray-200"
-                      onClick={() => goToPage(currentPage - 1)}
-                      disabled={currentPage === 1}
-                    >
-                      <ChevronLeft />
-                    </button>
-                    {getPageNumbers().map((page, index) =>
-                      page === "..." ? (
-                        <span key={index} className="p-2">
-                          ...
-                        </span>
-                      ) : (
-                        <button
-                          key={index}
-                          className={`border p-2  hover:bg-gray-200 ${
-                            currentPage === page ? "bg-gray-300" : ""
-                          }`}
-                          onClick={() => goToPage(page)}
-                        >
-                          {page}
-                        </button>
-                      ),
-                    )}
-                    <button
-                      className="border p-2  hover:bg-gray-200"
-                      onClick={() => goToPage(currentPage + 1)}
-                      disabled={currentPage === totalPages}
-                    >
-                      <ChevronLeft className="rotate-180" />
-                    </button>
-                  </div>
+      <div className="flex w-full flex-col items-center overflow-x-auto rounded-md bg-white p-4 shadow-sm">
+        <table className="w-full gap-2">
+          <thead className="sticky top-0 bg-[#c0c1c3]">
+            <tr>
+              <th>{t("document_order")} #</th>
+              <th>{t("Date")}</th>
+              <th>{t("Client")}</th>
+              <th>{t("Total")}</th>
+              <th>{t("To pay")}</th>
+              <th>{t("Paid")}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allOrders.map((order, index) => (
+              <tr
+                key={order.id}
+                className={`cursor-pointer ${
+                  index % 2 === 0 ? "bg-slate-300" : ""
+                }`}
+                onClick={() => router.push(`/admin/order?id=${order.id}`)}
+                onMouseOver={(e) =>
+                  e.currentTarget.classList.add("hover:bg-slate-500")
+                }
+                onMouseOut={(e) =>
+                  e.currentTarget.classList.remove("hover:bg-slate-500")
+                }
+              >
+                <td>{order.prefix + order.number}</td>
+                <td>{formatDateAPIToBe(order.date)}</td>
+                <td>{`${order.client.firstName} ${order.client.lastName}`}</td>
+                <td>
+                  €{" "}
+                  {order.document_products
+                    .reduce((total, product) => total + product.subTotal, 0)
+                    .toFixed(2)
+                    .replaceAll(".", ",")}
+                </td>
+                <td>
+                  €{" "}
+                  {(
+                    order.document_products.reduce(
+                      (total, product) => total + product.subTotal,
+                      0,
+                    ) -
+                    order.payments.reduce(
+                      (total, payment) =>
+                        total +
+                        (payment.deleted || !payment.verified
+                          ? 0
+                          : payment.value),
+                      0,
+                    )
+                  )
+                    .toFixed(2)
+                    .replaceAll(".", ",")}
+                </td>
+                <td>
+                  {order.document_products.reduce(
+                    (total, product) => total + product.subTotal,
+                    0,
+                  ) -
+                    order.payments.reduce(
+                      (total, payment) =>
+                        total +
+                        (payment.deleted || !payment.verified
+                          ? 0
+                          : payment.value),
+                      0,
+                    ) >
+                  0 ? (
+                    <X className="text-red-500" />
+                  ) : (
+                    <Check className="text-green-500" />
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <>
+          {allOrders.length > 0 ? (
+            <div className="mb-4 flex flex-row justify-center px-6">
+              <div className="mt-2">
+                <div className="flex items-center justify-center space-x-1">
+                  <button
+                    className="border p-2  hover:bg-gray-200"
+                    onClick={() => goToPage(currentPage - 1)}
+                    disabled={currentPage === 1}
+                  >
+                    <ChevronLeft />
+                  </button>
+                  {getPageNumbers().map((page, index) =>
+                    page === "..." ? (
+                      <span key={index} className="p-2">
+                        ...
+                      </span>
+                    ) : (
+                      <button
+                        key={index}
+                        className={`border p-2  hover:bg-gray-200 ${
+                          currentPage === page ? "bg-gray-300" : ""
+                        }`}
+                        onClick={() => goToPage(page)}
+                      >
+                        {page}
+                      </button>
+                    ),
+                  )}
+                  <button
+                    className="border p-2  hover:bg-gray-200"
+                    onClick={() => goToPage(currentPage + 1)}
+                    disabled={currentPage === totalPages}
+                  >
+                    <ChevronLeft className="rotate-180" />
+                  </button>
                 </div>
               </div>
-            ) : (
-              <p></p>
-            )}
-          </>
-        </div>
+            </div>
+          ) : (
+            <p></p>
+          )}
+        </>
       </div>
     </AdminLayout>
   );
