62a13f41421293e48fa74e3d0f15f7800dd10f68
fjhus16 <65453275+huseyinonalcom@users.noreply.github.com>
2024-07-20 16:32:17 +0300

    s

--- components/public/meta.tsx
+++ components/public/meta.tsx
@@ -28,6 +28,11 @@ const Meta = () => {
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#ffffff" />
+      <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5DMMM9F');`}</script>
     </>
   );
 };
