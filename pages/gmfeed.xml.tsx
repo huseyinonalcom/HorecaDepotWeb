2da1da60065e06bb55bbde94bfe76b4b4f82e193
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-07-29 11:07:26 +0300

    Update gmfeed.xml.tsx

--- pages/gmfeed.xml.tsx
+++ pages/gmfeed.xml.tsx
@@ -14,7 +14,7 @@ ${products
 <item>
 <g:id>${prd.internalCode}</g:id>
 <g:title>${prd.categories.at(0).Name + " " + prd.name}</g:title>
-<g:description>${prd.description ?? prd.categories.map((c) => c.Name).join(", ")}</g:description>
+<g:description>${prd.name + " " + prd.description ?? prd.categories.map((c) => c.Name).join(", ")}</g:description>
 <g:link>https://www.horecadepot.be/products/${prd.categories.at(0).Name}/${prd.name}/${prd.id}</g:link> <g:image_link>https://hdapi.huseyinonalalpha.com${prd.images.at(0).url}</g:image_link> <g:condition>new</g:condition>
 <g:availability>in stock</g:availability>
 <g:price>${prd.value} EUR</g:price>
@@ -25,6 +25,15 @@ ${products
 <g:service>Standard</g:service>
 <g:price>20 EUR</g:price>
 
+<g:country>FR</g:country>
+<g:service>Standard</g:service>
+<g:price>100 EUR</g:price>
+<g:country>NL</g:country>
+<g:service>Standard</g:service>
+<g:price>100 EUR</g:price>
+<g:country>DE</g:country>
+<g:service>Standard</g:service>
+<g:price>100 EUR</g:price>
 </g:shipping>
 <g:brand>HorecaDepot</g:brand>
 
