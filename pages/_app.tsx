77f6c30fed764685e957d4a861e8e58aaf1f283a
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-07-31 00:57:36 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import Script from "next/script";
 import "../global.css";
 import { CategoryProvider } from "../api/providers/categoryProvider";
 import { AdminDrawerProvider } from "../api/providers/adminDrawerProvider";
@@ -7,6 +8,22 @@ import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-3R8DQVPZVD"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3R8DQVPZVD);
+        `,
+        }}
+      />
       <GoogleTagManager gtmId="GTM-5DMMM9F" />
       <GoogleAnalytics gaId="167998249" />
       <CategoryProvider>
