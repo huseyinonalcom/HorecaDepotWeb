94368768079cb9928a91e29ee21a51e59a06d923
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-08-02 00:17:20 +0300

    Update header.tsx

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -941,22 +941,13 @@ const Header = () => {
                 <Menu className="mb-[1px]" />
                 <p className="font-semibold">{t("CATEGORIES")}</p>
               </button>
-              {/* <div className="hidden flex-row items-center gap-2 whitespace-nowrap text-lg text-yellow-400 lg:flex">
-                <GiBulldozer size={42} className="text-yellow-400" />
-                <a href="https://api.whatsapp.com/send?phone=32499738373" className="flex flex-col items-center">
-                  <p>Website</p> <p>under</p> <p>construction</p>
-                </a>
-              </div> */}
             </div>
             <div className="flex h-[45px] flex-row gap-2 md:ml-48 md:w-full">
               <DesktopSearch />
               <HeaderButtons cartItems={cartItems} />
             </div>
           </div>
-          <a href="https://api.whatsapp.com/send?phone=32499738373" className="mx-auto flex w-full flex-row items-center justify-center gap-2 whitespace-nowrap text-lg text-yellow-400 lg:hidden">
-            <GiBulldozer size={42} className="text-yellow-400" />
-            Website under construction
-          </a>
+
           <div className="flex w-full flex-row md:hidden">
             <div className="relative mt-3 w-full">
               <MobileSearch />
