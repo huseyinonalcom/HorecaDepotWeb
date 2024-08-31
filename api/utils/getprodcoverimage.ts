23b81b40aee34ea4c332326396d875f4c5de41ed
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-08-31 23:05:51 +0300

    Revert "Merge branch 'main' of https://github.com/huseyinonalcom/HorecaDepotWeb"
    This reverts commit d18d49f0b34979a456e9c51c56063c64810e1bc3, reversing
    changes made to ff03559e3789979bca81b37f29bcc63f606c721b.

--- api/utils/getprodcoverimage.ts
+++ api/utils/getprodcoverimage.ts
@@ -2,7 +2,7 @@ import { Product } from "../interfaces/product";
 
 export const getCoverImageUrl = (prd: Product) => {
   if (prd.imageDirections && prd.imageDirections.fr != 0) {
-    return prd.images.find((img) => img.id == prd.imageDirections.fr)?.url ?? "/uploads/placeholder_9db455d1f1.webp";
+    return prd.images.find((img) => img.id == prd.imageDirections.fr).url;
   } else {
     return prd.images.at(0)?.url ?? "/uploads/placeholder_9db455d1f1.webp";
   }
