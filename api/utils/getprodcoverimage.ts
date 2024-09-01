5998e83feb13007da5a4147cf45e4556a0dec8bb
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-09-02 00:48:34 +0300

    Update getprodcoverimage.ts

--- api/utils/getprodcoverimage.ts
+++ api/utils/getprodcoverimage.ts
@@ -1,8 +1,13 @@
 import { Product } from "../interfaces/product";
 
 export const getCoverImageUrl = (prd: Product) => {
+  console.log(prd);
   if (prd.imageDirections && prd.imageDirections.fr != 0) {
-    return prd.images.find((img) => img.id == prd.imageDirections.fr)?.url ?? "/uploads/placeholder_9db455d1f1.webp";
+    return (
+      prd.images.find((img) => img.id == prd.imageDirections.fr)?.url ??
+      prd.images.at(0)?.url ??
+      "/uploads/placeholder_9db455d1f1.webp"
+    );
   } else {
     return prd.images.at(0)?.url ?? "/uploads/placeholder_9db455d1f1.webp";
   }
