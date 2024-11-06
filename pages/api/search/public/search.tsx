bc8e5068a1f4c9bff693245fc0d2db206cf5dbf0
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-11-07 00:01:27 +0300

    Update search.tsx

--- pages/api/search/public/search.tsx
+++ pages/api/search/public/search.tsx
@@ -121,7 +121,6 @@ export async function fuzzySearch({ search }: { search: string }) {
   }
 }
 
-// API route handler for fetching projects
 export default async function handler(req, res) {
   if (req.method !== "GET") {
     return res.status(405).json(statusText[405]);
