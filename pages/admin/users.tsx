3160e825743b2cd17a9d6beb6b4b10d9daf334b9
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2025-03-04 17:43:45 +0300

    d

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -15,7 +15,7 @@ export default function Users(props) {
       </Head>
       <div className="flex w-full flex-col items-center overflow-x-auto rounded-md bg-white p-4 shadow-sm">
         <table className="w-full gap-2">
-          <thead className="sticky top-0 bg-[#c0c1c3]">
+          <thead>
             <tr>
               <th>{t("name")}</th>
               <th>{t("email")}</th>
@@ -24,6 +24,63 @@ export default function Users(props) {
             </tr>
           </thead>
           <tbody>
+            {props.allUsers.map((user, index) => (
+              <tr
+                key={user.id}
+                className={`cursor-pointer ${
+                  index % 2 === 0 ? "bg-slate-300" : ""
+                }`}
+                onMouseOver={(e) =>
+                  e.currentTarget.classList.add("hover:bg-slate-500")
+                }
+                onMouseOut={(e) =>
+                  e.currentTarget.classList.remove("hover:bg-slate-500")
+                }
+              >
+                <td>{user.user_info.firstName}</td>
+                <td>{user.email}</td>
+                <td>{rankFromRole(user.role.name)}</td>
+                <td>{t(user.blocked ? "blocked" : "active")}</td>
+              </tr>
+            ))}
+            {props.allUsers.map((user, index) => (
+              <tr
+                key={user.id}
+                className={`cursor-pointer ${
+                  index % 2 === 0 ? "bg-slate-300" : ""
+                }`}
+                onMouseOver={(e) =>
+                  e.currentTarget.classList.add("hover:bg-slate-500")
+                }
+                onMouseOut={(e) =>
+                  e.currentTarget.classList.remove("hover:bg-slate-500")
+                }
+              >
+                <td>{user.user_info.firstName}</td>
+                <td>{user.email}</td>
+                <td>{rankFromRole(user.role.name)}</td>
+                <td>{t(user.blocked ? "blocked" : "active")}</td>
+              </tr>
+            ))}
+            {props.allUsers.map((user, index) => (
+              <tr
+                key={user.id}
+                className={`cursor-pointer ${
+                  index % 2 === 0 ? "bg-slate-300" : ""
+                }`}
+                onMouseOver={(e) =>
+                  e.currentTarget.classList.add("hover:bg-slate-500")
+                }
+                onMouseOut={(e) =>
+                  e.currentTarget.classList.remove("hover:bg-slate-500")
+                }
+              >
+                <td>{user.user_info.firstName}</td>
+                <td>{user.email}</td>
+                <td>{rankFromRole(user.role.name)}</td>
+                <td>{t(user.blocked ? "blocked" : "active")}</td>
+              </tr>
+            ))}
             {props.allUsers.map((user, index) => (
               <tr
                 key={user.id}
