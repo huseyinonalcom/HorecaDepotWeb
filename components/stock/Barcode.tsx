dd42e0ef8ea20ee0c6b23f7eb86adf02b35a0f21
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2025-03-06 13:52:43 +0300

    Update Barcode.tsx

--- components/stock/Barcode.tsx
+++ components/stock/Barcode.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useZxing } from "react-zxing";
 
 export const BarcodeScanner = ({
@@ -5,13 +6,17 @@ export const BarcodeScanner = ({
 }: {
   onScan: (result: string) => void;
 }) => {
-  const audio = new Audio("/assets/sounds/beep.mpeg");
+  let audio;
 
   const beep = () => {
     try {
       audio.play().catch((e) => console.error("Playback failed:", e));
     } catch (_) {}
   };
+
+  useEffect(() => {
+    audio = new Audio("/assets/sounds/beep.mpeg");
+  }, []);
 
   const { ref } = useZxing({
     onDecodeResult(result) {
