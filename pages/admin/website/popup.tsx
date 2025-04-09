a15dfbe3a8d51459bd851377dfde949c23a1fa86
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2025-04-09 09:46:19 +0300

    Update popup.tsx

--- pages/admin/website/popup.tsx
+++ pages/admin/website/popup.tsx
@@ -1,31 +1,28 @@
-import { useEffect, useState } from "react";
-import Head from "next/head";
-import useTranslation from "next-translate/useTranslation";
+import AdminPanelLayout from "../../../components/admin/AdminPanelLayout";
 import componentThemes from "../../../components/componentThemes";
 import InputOutlined from "../../../components/inputs/outlined";
+import StyledForm from "../../../components/form/StyledForm";
 import ImageWithURL from "../../../components/common/image";
-import { useRouter } from "next/router"; 
-import AdminPanelLayout from "../../../components/admin/AdminPanelLayout";
+import useTranslation from "next-translate/useTranslation";
+import { PhotoIcon } from "@heroicons/react/24/outline";
 import { FiCheck, FiX } from "react-icons/fi";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import InputField from "../../../components/form/InputField";
+import { getPopup } from "../../api/popup/getpopup";
+import {
+  Description,
+  Field,
+  Fieldset,
+} from "../../../components/styled/fieldset";
 
-export default function PopupAdmin() {
+export default function PopupAdmin(props) {
   const router = useRouter();
-  const { t, lang } = useTranslation("common");
-  const [popup, setPopup] = useState(null);
+  const { t } = useTranslation("common");
+  const [popup, setPopup] = useState(props.popup);
 
-  const fetchCategories = async () => {
-    const fetchWebsiteRequest = await fetch(`/api/popup/getpopup`, {
-      method: "GET",
-    });
-    if (fetchWebsiteRequest.ok) {
-      const fetchWebsiteRequestAnswer = await fetchWebsiteRequest.json();
-      return fetchWebsiteRequestAnswer;
-    } else {
-      return null;
-    }
-  };
-
-  const putCategories = async () => {
+  const putPopup = async () => {
     const putWebsiteRequest = await fetch("/api/popup/putpopup", {
       method: "PUT",
       body: JSON.stringify({
@@ -40,25 +37,13 @@ export default function PopupAdmin() {
     }
   };
 
-  useEffect(() => {
-    if (!popup) {
-      const fetchAndSetCategories = async () => {
-        await fetchCategories().then((res) => {
-          setPopup(res.data);
-          return res;
-        });
-      };
-      fetchAndSetCategories();
-    }
-  }, []);
-
   const uploadFiles = async (e) => {
     e.preventDefault();
     e.stopPropagation();
 
     if (e.target.files && e.target.files[0]) {
       const files = e.target.files;
-      await sendFile(files).then((res) => (e.target.value = ""));
+      await sendFile(files).then(() => (e.target.value = ""));
     }
   };
 
@@ -92,65 +77,89 @@ export default function PopupAdmin() {
       <Head>
         <title>{t("popup")}</title>
       </Head>
-      <div className="mx-auto flex w-[95%] flex-col items-center p-2">
-        {popup && (
-          <div className="grid w-full grid-cols-1 gap-2">
-            <div className="flex flex-col gap-2">
-              <InputOutlined
+      {popup && (
+        <StyledForm onSubmit={(e) => e.preventDefault()}>
+          <Fieldset>
+            <Field>
+              <InputField
+                name="url"
                 label={t("url")}
-                value={popup.url}
-                onChange={(e) => {
-                  setPopup({ ...popup, url: e.target.value });
-                }}
+                defaultValue={popup.url}
               />
-              <div className="text flex flex-row text-green-600">
-                <p>https://horecadepot.be/shop/...</p>
-                <FiCheck />
-              </div>
-
-              <div className="text flex flex-row text-red-600">
-                <p>horecadepot.be/shop/...</p>
-                <FiX />
-              </div>
-              <input type="file" onChange={uploadFiles} />
-              {popup.img && (
-                <div className="grid grid-cols-1 gap-2">
-                  <div className="flex flex-col gap-2">
-                    <ImageWithURL
-                      src={popup.img}
-                      alt={""}
-                      height={1000}
-                      width={1000}
-                    />
-                    <button
-                      onClick={() => {
-                        setPopup({
-                          ...popup,
-                          img: null,
-                        });
-                      }}
-                      className={componentThemes.outlinedButton}
-                    >
-                      {t("delete")}
-                    </button>
-                  </div>
+              <Description>
+                <div className="text flex flex-row items-center text-green-600">
+                  <p>https://horecadepot.be/shop/...</p>
+                  <FiCheck />
                 </div>
-              )}
+
+                <div className="text flex flex-row items-center text-red-600">
+                  <p>horecadepot.be/shop/...</p>
+                  <FiX />
+                </div>
+              </Description>
+            </Field>
+          </Fieldset>
+          <div className="col-span-full">
+            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+              <div className="text-center">
+                <PhotoIcon
+                  aria-hidden="true"
+                  className="mx-auto size-12 text-gray-300"
+                />
+                <div className="mt-4 flex text-sm/6 text-gray-600">
+                  <label
+                    htmlFor="file-upload"
+                    className="focus-within:outline-hidden relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>{t("upload-file")}</span>
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      type="file"
+                      className="sr-only"
+                      onChange={uploadFiles}
+                      onDrop={uploadFiles}
+                    />
+                  </label>
+                  <p className="pl-1">{t("or-drag-drop")}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {popup.img && (
+            <div className="flex flex-col gap-2">
+              <ImageWithURL
+                src={popup.img}
+                alt={""}
+                height={1000}
+                width={1000}
+              />
               <button
-                onClick={async () => {
-                  const response = await putCategories();
-                  if (response == true) {
-                    router.reload();
-                  }
+                onClick={() => {
+                  setPopup({
+                    ...popup,
+                    img: null,
+                  });
                 }}
                 className={componentThemes.outlinedButton}
               >
-                {t("save")}
+                {t("delete")}
               </button>
             </div>
-          </div>
-        )}
-      </div>
+          )}
+          <button
+            onClick={async () => {
+              const response = await putPopup();
+              if (response == true) {
+                router.reload();
+              }
+            }}
+            className={componentThemes.outlinedButton}
+          >
+            {t("save")}
+          </button>
+        </StyledForm>
+      )}
     </>
   );
 }
@@ -159,3 +168,12 @@ PopupAdmin.getLayout = function getLayout(children) {
   const { t } = useTranslation("common");
   return <AdminPanelLayout title={t("popup")}>{children}</AdminPanelLayout>;
 };
+
+export async function getServerSideProps() {
+  const popup = (await getPopup()).data;
+  return {
+    props: {
+      popup,
+    },
+  };
+}
