bcf1749f95b22e7b2f3a3b622cbdc7c5b0be03ab
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2025-04-22 02:15:00 +0300

    Update [id].tsx

--- pages/admin/reservation/[id].tsx
+++ pages/admin/reservation/[id].tsx
@@ -1,10 +1,5 @@
-import { FiSearch, FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import StockLayout from "../../../components/stock/StockLayout";
-import ProductCard from "../../../components/stock/ProductCard";
 import useTranslation from "next-translate/useTranslation";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import Link from "next/link";
 import Head from "next/head";
 
 export default function ReservationAdmin() {
@@ -16,6 +11,7 @@ export default function ReservationAdmin() {
         <title>{t("reservation")}</title>
         <meta name="language" content={lang} />
       </Head>
+      <h1>{t("reservation")}</h1>
     </>
   );
 }
