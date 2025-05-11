60cd470ea5527d29fc52b7f79450ee30cee7c12b
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2025-05-11 05:11:09 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -441,7 +441,7 @@ export default function Index({
         <div className="flex w-full flex-col items-center">
           <div
             id="slider-1"
-            className={`no-scrollbar flex w-full snap-x snap-mandatory flex-row overflow-x-scroll z-0`}
+            className={`no-scrollbar z-0 flex w-full snap-x snap-mandatory flex-row overflow-x-scroll`}
           >
             <DndContext
               sensors={[
