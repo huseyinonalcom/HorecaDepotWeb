28f846b6e04679404059ae382aeca8430dc53a6f
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2025-06-23 16:35:31 +0300

    Update payment.tsx

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
-import useTranslation from "next-translate/useTranslation";
-import { useRouter } from "next/router";
 import LoadingIndicator from "../components/common/loadingIndicator";
+import useTranslation from "next-translate/useTranslation";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function PaymentVerification() {
   const { t } = useTranslation("common");
   const router = useRouter();
 
-  const [paymentCheckDone, setPaymentCheckDone] = useState(false);
   const [paymentSuccessful, setPaymentSuccessful] = useState(false);
+  const [paymentCheckDone, setPaymentCheckDone] = useState(false);
   const [paymentID, setPaymentID] = useState(null);
 
   useEffect(() => {
@@ -18,14 +18,15 @@ export default function PaymentVerification() {
         setPaymentID(paymentID);
 
         const verifyRequest = async () => {
-          const fetchUrl = `/api/payment/verifypayment?id=${docID}`;
-          const request = await fetch(fetchUrl, {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json",
+          const request = await fetch(
+            `/api/payment/verifypayment?id=${docID}`,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+              },
             },
-          });
+          );
           if (request.ok) {
             const answer = await request.json();
             if (answer.paymentSuccess) {
@@ -51,14 +52,12 @@ export default function PaymentVerification() {
   }, [router.isReady]);
 
   if (!paymentCheckDone) {
-    // show something to indicate something is running in the background
     return (
       <div className="flex h-[90vh] w-[90vw] flex-col items-center justify-center">
         <LoadingIndicator label={t("Payment being checked")} />
       </div>
     );
   } else if (!paymentSuccessful) {
-    // give choice between re-attempting payment or going to the order page (find order by fetching order related to payment)
     return (
       <div className="flex h-[90vh] w-[90vw] flex-col items-center justify-center">
         <h2>{t("payment_fail")}</h2>
@@ -66,7 +65,6 @@ export default function PaymentVerification() {
     );
   } else {
     localStorage.removeItem("cart");
-    // show something to indicate payment succeeded and redirect to the order page (find order by fetching order related to payment)
     return (
       <div className="flex h-[90vh] w-[90vw] flex-col items-center justify-center">
         <h2>{t("payment_success")}</h2>
