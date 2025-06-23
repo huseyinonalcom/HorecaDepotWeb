4bb8ce1c684c5551fe3be7cc2117c37abd044adb
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2025-06-23 17:48:45 +0300

    Update checkout.tsx

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -967,7 +967,7 @@ export default function Checkout() {
                   </p>
                 </div>
                 <div>
-                  <p >
+                  <p>
                     {t("wrong_user", {
                       userName: `${client.client_info.firstName} ${client.client_info.lastName}`,
                     })}
