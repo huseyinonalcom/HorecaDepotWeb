b633f0842a87a7b8a54ebff1c0819948a5973cef
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2025-06-23 16:43:51 +0300

    Update ProductSelector.tsx

--- components/selector/ProductSelector.tsx
+++ components/selector/ProductSelector.tsx
@@ -1,4 +1,3 @@
-import ProductPreviewCustom from "../products/product-preview-custom";
 import useTranslation from "next-translate/useTranslation";
 import { Product } from "../../api/interfaces/product";
 import { useEffect, useState, useMemo } from "react";
@@ -6,6 +5,7 @@ import { FieldGroup } from "../styled/fieldset";
 import debounce from "../../api/utils/debounce";
 import { Input } from "../styled/input";
 import { Paging } from "../paging";
+import { ProductPreviewCustom } from "../products/product-preview-custom";
 
 const fetchProducts = async ({
   search,
