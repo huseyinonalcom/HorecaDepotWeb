4926786fbdbb4c5a1367da7d1559f1970e3eb84f
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2025-07-29 11:47:21 +0300

    Update collection.tsx

--- pages/admin/website/collection.tsx
+++ pages/admin/website/collection.tsx
@@ -4,8 +4,12 @@ import AdminPanelLayout from "../../../components/admin/AdminPanelLayout";
 import componentThemes from "../../../components/componentThemes";
 import ButtonShadow1 from "../../../components/buttons/shadow_1";
 import InputOutlined from "../../../components/inputs/outlined";
+import StyledForm from "../../../components/form/StyledForm";
+import { Divider } from "../../../components/styled/divider";
 import ImageWithURL from "../../../components/common/image";
 import useTranslation from "next-translate/useTranslation";
+import { Button } from "../../../components/styled/button";
+import { FiCopy, FiTrash } from "react-icons/fi";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
@@ -66,6 +70,136 @@ export default function Collection() {
     } else setIsLoading(false);
   }, [router.isReady, router.query.id]);
 
+  const postCollection = async (e) => {
+    e.preventDefault();
+    const request = await fetch(`/api/collections/admin/submitcollection`, {
+      method: "POST",
+      body: JSON.stringify({ name: currentCollection.name }),
+    });
+    const answer = await request.json();
+    if (request.ok) {
+      router.push(`/admin/website/collection?id=${answer.id}`);
+    } else {
+      setSubmitError(t("collection_submit_error"));
+    }
+  };
+
+  const putCollection = async (e) => {
+    e.preventDefault();
+    const request = await fetch(
+      `/api/collections/admin/submitcollection?id=${currentCollection.id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          name: currentCollection.name,
+          products: currentCollection.products.map((pro) => pro.id),
+        }),
+      },
+    );
+    if (request.ok) {
+      router.push("/admin/website/collections");
+    } else {
+      setSubmitError(t("collection_modify_error"));
+    }
+  };
+
+  const deleteCollection = async () => {
+    const deleteColl = async () => {
+      try {
+        const request = await fetch(
+          `/api/private/collections/collections?id=` + currentCollection.id,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          },
+        );
+        if (!request.ok) {
+        } else {
+          router.push("/admin/website/collections");
+        }
+      } catch {}
+    };
+
+    let confirmDelete = confirm(t("confirm_delete"));
+
+    if (confirmDelete) {
+      deleteColl();
+    }
+  };
+
+  const handleFormSubmit = async (e) => {
+    console.log(currentCollection);
+
+    e.preventDefault();
+    if (isLoading) return;
+    try {
+      setIsLoading(true);
+      setSubmitError(null);
+
+      if (
+        !currentCollection.products ||
+        currentCollection.products.length == 0
+      ) {
+        setSubmitError("validators_empty_invalid");
+        setIsLoading(false);
+        return;
+      }
+
+      if (currentCollection.id != 0) {
+        try {
+          const request = await fetch(
+            `/api/collections/admin/putcollection?id=` + currentCollection.id,
+            {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(currentCollection),
+            },
+          );
+          const ans = await request.text();
+          if (!request.ok) {
+            console.error("Error updating collection:", ans);
+            setIsLoading(false);
+            setSubmitError(ans);
+          } else {
+          }
+        } catch (e) {
+          setIsLoading(false);
+          setSubmitError(
+            t("An error occurred while modifying the collection!"),
+          );
+        }
+      } else {
+        try {
+          const request = await fetch("/api/collections/admin/postcollection", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(currentCollection),
+          });
+          if (!request.ok) {
+            const answer = await request.text();
+            setIsLoading(false);
+            setSubmitError(answer);
+          } else {
+          }
+        } catch (e) {
+          setIsLoading(false);
+          setSubmitError(
+            t("An error occurred during the collection creation!"),
+          );
+        }
+      }
+    } catch (error) {
+      setIsLoading(false);
+      setSubmitError(error);
+    }
+  };
+
   if (isLoading) {
     return (
       <>
@@ -80,79 +214,58 @@ export default function Collection() {
       </>
     );
   } else if (!currentCollection) {
-    const postCollection = async (e) => {
-      e.preventDefault();
-      const request = await fetch(`/api/collections/admin/submitcollection`, {
-        method: "POST",
-        body: JSON.stringify({ name: currentCollection.name }),
-      });
-      const answer = await request.json();
-      if (request.ok) {
-        router.push(`/admin/website/collection?id=${answer.id}`);
-      } else {
-        setSubmitError(t("collection_submit_error"));
-      }
-    };
     return (
       <>
         <Head>
           <title>{t("collection")}</title>
         </Head>
-        <div className="mx-auto flex w-[95vw] flex-col items-center justify-start">
-          <div className="w-full py-2 text-center text-xl font-semibold">
-            {t("collection_name_choose")}
+        <StyledForm
+          bottomBarChildren={
+            <>
+              {currentCollection.id != 0 && (
+                <>
+                  <div className="flex flex-row gap-6">
+                    <Button
+                      type="button"
+                      color="red"
+                      onClick={deleteCollection}
+                    >
+                      <div className="p-2">
+                        <FiTrash />
+                      </div>
+                    </Button>
+                    <Button
+                      type="button"
+                      onClick={() => {
+                        setCurrentCollection((pr) => ({
+                          ...pr,
+                          id: 0,
+                        }));
+                      }}
+                      color="white"
+                    >
+                      <div className="p-2">
+                        <FiCopy />
+                      </div>
+                    </Button>
+                  </div>
+                </>
+              )}
+              {submitError && (
+                <p className="bg-white p-1 text-red-400">
+                  {submitError?.toString()}
+                </p>
+              )}
+            </>
+          }
+          onSubmit={handleFormSubmit}
+        >
+          <div className="hidden flex-row space-x-6 lg:flex">
+            <div className="flex w-full flex-col space-y-12 border-r border-zinc-950/10 pr-6">
+              <Divider />
+            </div>
           </div>
-          <form onSubmit={postCollection}>
-            <InputOutlined
-              type="text"
-              id="name"
-              required
-              value={currentCollection?.name ?? ""}
-              onChange={(e) =>
-                setCurrentCollection((pc) => ({
-                  ...pc,
-                  name: e.target.value,
-                }))
-              }
-              label={t("Name")}
-            />
-            <button className={componentThemes.outlinedButton}>
-              {t("collection_create")}
-            </button>
-            {submitError && (
-              <p className="text-center font-medium text-red-600">
-                {submitError}
-              </p>
-            )}
-          </form>
-        </div>
-      </>
-    );
-  } else {
-    const putCollection = async (e) => {
-      e.preventDefault();
-      const request = await fetch(
-        `/api/collections/admin/submitcollection?id=${currentCollection.id}`,
-        {
-          method: "PUT",
-          body: JSON.stringify({
-            name: currentCollection.name,
-            products: currentCollection.products.map((pro) => pro.id),
-          }),
-        },
-      );
-      if (request.ok) {
-        router.push("/admin/website/collections");
-      } else {
-        setSubmitError(t("collection_modify_error"));
-      }
-    };
-
-    return (
-      <>
-        <Head>
-          <title>{t("collection")}</title>
-        </Head>
+        </StyledForm>
         <div className="mx-auto flex w-full flex-col items-center justify-start">
           <div className="w-full py-2 text-center text-xl font-semibold">
             {currentCollection?.name} -
