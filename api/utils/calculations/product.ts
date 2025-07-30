d8a68767b28f7ffebb4503a6c651ed31505ac425
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2025-07-30 12:36:35 +0300

    Comment out supplier order products stock calculation
    The calculation of total received stock from supplier order products has been commented out in calculateProductStock. Supplier Orders are not ready yet.

--- api/utils/calculations/product.ts
+++ api/utils/calculations/product.ts
@@ -9,14 +9,14 @@ export function calculateProductStock(product: Product): number {
     let initialStock: number = 0;
     product.shelves.forEach((shelf) => (initialStock += shelf.stock));
     let totalReceieved: number = 0;
-    if (
+   /*  if (
       product.shelves.some((shelf) => shelf.supplier_order_products.length > 0)
     ) {
       product.shelves.forEach(
         (shelf) =>
           (totalReceieved += shelf.supplier_order_products["amountReceived"]),
       );
-    }
+    } */
     let totalSold: number = 0;
     if (product.document_products) {
       product.document_products.forEach(
